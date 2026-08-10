fe3a94091347509052b7440ad90e2e48998ea71b
Marcus <marcus@turboio.com>
2026-08-10 09:40:02 -0600

    release: v0.6.1

--- include/version.h
+++ include/version.h
@@ -7,7 +7,7 @@
 
 namespace fw {
 
-inline constexpr const char* kVersion = "0.6.0";
+inline constexpr const char* kVersion = "0.6.1";
 
 #ifdef FW_GIT_REV
 inline constexpr const char* kGitRev = FW_GIT_REV;
