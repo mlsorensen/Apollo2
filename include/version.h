70cb7e0c94e32cbbc2bdbd65d1ab917a81c1d1b5
Marcus <marcus@turboio.com>
2026-09-23 10:12:26 -0600

    version: 1.0.0-beta.3

--- include/version.h
+++ include/version.h
@@ -7,7 +7,7 @@
 
 namespace fw {
 
-inline constexpr const char* kVersion = "1.0.0-beta.2";
+inline constexpr const char* kVersion = "1.0.0-beta.3";
 
 #ifdef FW_GIT_REV
 inline constexpr const char* kGitRev = FW_GIT_REV;
